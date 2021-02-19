963914880111aca40779f92f997ef8e71faaeec2
shreyaskar123 <47864384+shreyaskar123@users.noreply.github.com>
2021-02-18 23:33:59 -0500

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -9,4 +9,11 @@ def create_plot(param_values, title_value, entity_value, figure_title):
         plt.savefig('fig_' + title_value.lower().replace(' ', '_') + '.png', dpi=300)
     
     return param_plot
+# Create individual plots
+create_plot(['accX', 'accY', 'accZ'], 'Acceleration', 'G','Acceleration(G) vs Time(seconds)')
+create_plot(['roll', 'pitch', 'yaw'], 'Rotation', 'degrees','Rotation(degrees) vs Time(seconds)')
+create_plot(['rotX', 'rotY', 'rotZ'], 'Rotation speed', 'd / second','Rotation Speed (d/second) vs Time(seconds)')
+if pref_plot_gravity:
+    create_plot(['gravX', 'gravY', 'gravZ'], 'Gravity', 'G','NULL')
+
    
